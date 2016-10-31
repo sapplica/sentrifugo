645d775f7cc26fca9f724c36ff40556e128cd494
go-faustino <go-faustino@users.noreply.github.com>
2016-10-31 16:46:27 +0000

    Relax client name validation
    Remove client name content validation

--- application/modules/timemanagement/forms/Clients.php
+++ application/modules/timemanagement/forms/Clients.php
@@ -42,12 +42,6 @@ class Timemanagement_Form_Clients extends Zend_Form
         $clientname->addFilter(new Zend_Filter_StringTrim());
         $clientname->setRequired(TRUE);
         $clientname->addValidator('NotEmpty', false, array('messages' => 'Please enter client name.'));  
-		$clientname->addValidator("regex",true,array(                           
-                           'pattern'=>'/^(?![0-9]*$)[a-zA-Z0-9.,&\(\)\/\-_\' ?]+$/',
-                           'messages'=>array(
-                               'regexNotMatch'=>'Please enter a valid client name.'
-                           )
-        			));
 
         $phonenumber = new Zend_Form_Element_Text('phone_no');
         $phonenumber->addFilter(new Zend_Filter_StringTrim());
