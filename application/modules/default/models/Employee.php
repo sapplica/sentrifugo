f2fb2fa4cc8753a710719996eb90c16b393c5eee
go-faustino <go-faustino@users.noreply.github.com>
2016-11-17 22:18:32 +0000

    Service request executors
    Include management in service request executor list

--- application/modules/default/models/Employee.php
+++ application/modules/default/models/Employee.php
@@ -499,7 +499,7 @@ class Default_Model_Employee extends Zend_Db_Table_Abstract
 	
 	public function getEmployeesForServiceDesk($bunitid='',$deptid='')
 	{
-		$where = 'e.isactive=1 AND r.group_id IN (2,3,4,6)';
+		$where = 'e.isactive=1 AND r.group_id IN (1,2,3,4,6)';
 		if($bunitid != '' && $bunitid !='null')
 			$where .= ' AND e.businessunit_id = '.$bunitid.'';
 		if($deptid !='' && $deptid !='null')
