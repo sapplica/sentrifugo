d714d7a067cf283631d879aa62ce0649a0513ca9
go-faustino <go-faustino@users.noreply.github.com>
2017-04-26 23:21:14 +0100

    undo excel fields

--- application/modules/timemanagement/models/Reports.php
+++ application/modules/timemanagement/models/Reports.php
@@ -664,12 +664,119 @@ class Timemanagement_Model_Reports extends Zend_Db_Table_Abstract
 	
 	function getBillingProjectReportsData($sort, $by, $pageNo, $perPage, $searchQuery, $start_date, $end_date, $empid, $param="",$flag=""){
 		
-		$db = Zend_Db_Table::getDefaultAdapter();
+		$andwhere = " AND (1=1)";
+		if($start_date != "")
+		{
+			if($end_date == "")
+			{
+				//$end_date = date('%Y-%m-%d %H:%i:%s');
+				$end_date = date('%Y-%m-%d');
+			}
+			$start_dates=strtotime($start_date);
+			$sd_month=date("m",$start_dates);
+			$sd_year=date("Y",$start_dates);
+			
+			$end_dates=strtotime($end_date);
+			$ed_month=date("m",$end_dates);
+			$ed_year=date("Y",$end_dates);
+			$andwhere = " AND et.ts_year >= ".$sd_year." AND et.ts_year <=".$ed_year." AND et.ts_month >= ".$sd_month." AND et.ts_month <= ".$ed_month;
+			
+			$duration="";
+			$duration_sort ="";
+			//$andwhere = " AND et.created BETWEEN STR_TO_DATE('".$start_date."','%Y-%m-%d %H:%i:%s') AND STR_TO_DATE('".$end_date."','%Y-%m-%d %H:%i:%s')";
+			if($param=="" || $param=="undefined" || $param=="Last 7 days")
+			{
+				$duration = "CONCAT(FLOOR(SUM( TIME_TO_SEC( IF(sun_date BETWEEN '".$start_date."' AND '".$end_date."',sun_duration,'00:00')) +
+				TIME_TO_SEC( IF(mon_date BETWEEN '".$start_date."' AND '".$end_date."',mon_duration,'00:00')) +
+				TIME_TO_SEC( IF(tue_date BETWEEN '".$start_date."' AND '".$end_date."',tue_duration,'00:00')) +
+				TIME_TO_SEC( IF(wed_date BETWEEN '".$start_date."' AND '".$end_date."',wed_duration,'00:00')) +
+				TIME_TO_SEC( IF(thu_date BETWEEN '".$start_date."' AND '".$end_date."',thu_duration,'00:00')) +
+				TIME_TO_SEC( IF(fri_date BETWEEN '".$start_date."' AND '".$end_date."',fri_duration,'00:00')) +
+				TIME_TO_SEC( IF(sat_date BETWEEN '".$start_date."' AND '".$end_date."',sat_duration,'00:00')))/3600),':',
+				LPAD(FLOOR(SUM( TIME_TO_SEC( IF(sun_date BETWEEN '".$start_date."' AND '".$end_date."',sun_duration,'00:00')) +
+				TIME_TO_SEC( IF(mon_date BETWEEN '".$start_date."' AND '".$end_date."',mon_duration,'00:00')) +
+				TIME_TO_SEC( IF(tue_date BETWEEN '".$start_date."' AND '".$end_date."',tue_duration,'00:00')) +
+				TIME_TO_SEC( IF(wed_date BETWEEN '".$start_date."' AND '".$end_date."',wed_duration,'00:00')) +
+				TIME_TO_SEC( IF(thu_date BETWEEN '".$start_date."' AND '".$end_date."',thu_duration,'00:00')) +
+				TIME_TO_SEC( IF(fri_date BETWEEN '".$start_date."' AND '".$end_date."',fri_duration,'00:00')) +
+				TIME_TO_SEC( IF(sat_date BETWEEN '".$start_date."' AND '".$end_date."',sat_duration,'00:00')))/60)%60,2,'0'))";
+				
+				$duration_sort = "TIME_TO_SEC( IF(mon_date BETWEEN '".$start_date."' AND '".$end_date."',mon_duration,'00:00')) +
+				TIME_TO_SEC( IF(tue_date BETWEEN '".$start_date."' AND '".$end_date."',tue_duration,'00:00')) +
+				TIME_TO_SEC( IF(wed_date BETWEEN '".$start_date."' AND '".$end_date."',wed_duration,'00:00')) +
+				TIME_TO_SEC( IF(thu_date BETWEEN '".$start_date."' AND '".$end_date."',thu_duration,'00:00')) +
+				TIME_TO_SEC( IF(fri_date BETWEEN '".$start_date."' AND '".$end_date."',fri_duration,'00:00')) +
+				TIME_TO_SEC( IF(sat_date BETWEEN '".$start_date."' AND '".$end_date."',sat_duration,'00:00'))";
+				
+				$andwhere =" AND (sun_date BETWEEN '".$start_date."' AND '".$end_date."' OR mon_date BETWEEN '".$start_date."' AND '".$end_date."'
+				OR tue_date BETWEEN '".$start_date."' AND '".$end_date."' OR wed_date BETWEEN '".$start_date."' AND '".$end_date."'
+				OR thu_date BETWEEN '".$start_date."' AND '".$end_date."' OR fri_date BETWEEN '".$start_date."' AND '".$end_date."'
+				OR sat_date BETWEEN '".$start_date."' AND '".$end_date."')";
+			}
+			else if($param=='Today')
+			{
+				$duration = "CONCAT(FLOOR(SUM( TIME_TO_SEC( IF(sun_date = '".$start_date."',sun_duration,'00:00')) +
+				TIME_TO_SEC( IF(mon_date = '".$start_date."' ,mon_duration,'00:00')) +
+				TIME_TO_SEC( IF(tue_date = '".$start_date."' ,tue_duration,'00:00')) +
+				TIME_TO_SEC( IF(wed_date = '".$start_date."' ,wed_duration,'00:00')) +
+				TIME_TO_SEC( IF(thu_date = '".$start_date."' ,thu_duration,'00:00')) +
+				TIME_TO_SEC( IF(fri_date = '".$start_date."' ,fri_duration,'00:00')) +
+				TIME_TO_SEC( IF(sat_date = '".$start_date."' ,sat_duration,'00:00')))/3600),':',
+				LPAD(FLOOR(SUM( TIME_TO_SEC( IF(sun_date = '".$start_date."' ,sun_duration,'00:00')) +
+				TIME_TO_SEC( IF(mon_date = '".$start_date."' ,mon_duration,'00:00')) +
+				TIME_TO_SEC( IF(tue_date = '".$start_date."' ,tue_duration,'00:00')) +
+				TIME_TO_SEC( IF(wed_date = '".$start_date."' ,wed_duration,'00:00')) +
+				TIME_TO_SEC( IF(thu_date = '".$start_date."' ,thu_duration,'00:00')) +
+				TIME_TO_SEC( IF(fri_date = '".$start_date."' ,fri_duration,'00:00')) +
+				TIME_TO_SEC( IF(sat_date = '".$start_date."' ,sat_duration,'00:00')))/60)%60,2,'0'))";
+				
+				$duration_sort = "TIME_TO_SEC( IF(mon_date = '".$start_date."' ,mon_duration,'00:00')) +
+				TIME_TO_SEC( IF(tue_date = '".$start_date."' ,tue_duration,'00:00')) +
+				TIME_TO_SEC( IF(wed_date = '".$start_date."' ,wed_duration,'00:00')) +
+				TIME_TO_SEC( IF(thu_date = '".$start_date."' ,thu_duration,'00:00')) +
+				TIME_TO_SEC( IF(fri_date = '".$start_date."' ,fri_duration,'00:00')) +
+				TIME_TO_SEC( IF(sat_date = '".$start_date."' ,sat_duration,'00:00'))";
+				
+				$andwhere = " AND (sun_date = '".$start_date."' OR mon_date = '".$start_date."' OR tue_date = '".$start_date."' OR wed_date = '".$start_date."' OR thu_date = '".$start_date."' OR fri_date = '".$start_date."' OR sat_date = '".$start_date."')";
+			}
+			else
+			{
+				$duration = "concat(floor(SUM( TIME_TO_SEC( et.week_duration ))/3600),':',lpad(floor(SUM( TIME_TO_SEC( et.week_duration ))/60)%60,2,'0'))";
+				$andwhere = " AND et.ts_year >= ".$sd_year." AND et.ts_year <=".$ed_year." AND et.ts_month >= ".$sd_month." AND et.ts_month <= ".$ed_month;
+				$duration_sort = "SUM(TIME_TO_SEC(et.week_duration))";
+			}
+		}
+		
+		if($searchQuery){
+			$andwhere .= " AND ".$searchQuery;	
+		}
+
+		if($empid != "")
+		{
+			$andwhere .= ' AND et.emp_id = '.$empid;
+		}
+		//'duration'=>'concat(floor(SUM( TIME_TO_SEC( et.week_duration ))/3600),":",lpad(floor(SUM( TIME_TO_SEC( et.week_duration ))/60)%60,2,"0"))'
+		//'SUM(TIME_TO_SEC(et.week_duration))'
 		$select = $this->select()
-			   		 	->setIntegrityCheck(false)
-					   	->from(array('e' => 'main_employees_summary'),array('e.Firstname','e.Lastname','e.businessunit_name','e.office_faxnumber'))  
-					   	->where('e.isactive <> 0 and user_id='.$empid);
-		return $this->fetchAll($select)->toArray();
+		->setIntegrityCheck(false)
+		->from(array('et' => 'tm_emp_timesheets'),array('p.project_name','proj_category'=>'p.project_type','p.id','project_type'=>'IF(p.project_type="billable","Billable (production)",IF(p.project_type="non_billable","Non billable","Billable (annual budget)"))',
+                                'duration'=>$duration,'duration_sort'=>$duration_sort))  
+		->joinInner(array('pt'=>'tm_project_tasks'), 'pt.id = et.project_task_id',array())
+		->joinInner(array('p'=>'tm_projects'), 'p.id = pt.project_id and p.id = et.project_id',array())
+		->joinInner(array('e'=>'main_employees_summary'), 'e.user_id = et.emp_id',array())
+		->joinLeft(array('pm'=>'tm_project_employees'), 'p.id = pm.project_id and pm.emp_id = et.emp_id ',array())
+		//->joinLeft(array('pm'=>new Zend_Db_Expr('(SELECT project_id,GROUP_CONCAT(emp_id) as manager_ids FROM tm_project_employees 
+		//WHERE is_active=1 and emp_type = \'manager\' GROUP BY project_id)')), 'pm.project_id = pt.project_id',array())
+		->where('et.is_active=1 '.$andwhere)
+		->order("$by $sort")
+		->group('p.id')
+		->limitPage($pageNo, $perPage);
+		if(!empty($flag))
+		{
+			return $this->fetchAll($select)->toArray(); 
+		}
+		//echo $select;//exit;
+		return $select;
 	}
 
 	public function getEmpProjDuration($empId,$start_date,$end_date,$project_id,$param)
