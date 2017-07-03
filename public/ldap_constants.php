9b72936037182d5e6d63e091cb4a00707e2230a1
Taras Shchybovyk <taras.shchybovyk@eleks.com>
2017-07-03 15:25:18 +0300

    updated ldap parameters

--- public/ldap_constants.php
+++ public/ldap_constants.php
@@ -1,14 +1,14 @@
 <?php
                defined('LDAP_ENABLED') || define('LDAP_ENABLED','true');
-	           defined('LDAP_HOST') || define('LDAP_HOST','localhost');
+	           defined('LDAP_HOST') || define('LDAP_HOST','172.25.3.236');
 	           defined('LDAP_PORT') || define('LDAP_PORT','389');
-	           defined('LDAP_USERNAME') || define('LDAP_USERNAME','CN=user.second,OU=users,DC=test,DC=com');
-	           defined('LDAP_PASSWORD') || define('LDAP_PASSWORD','12345');
-	           defined('LDAP_ACCOUNTFILTERFORMAT') || define('LDAP_ACCOUNTFILTERFORMAT','(&(objectClass=posixAccount)(cn=%s))');
-	           defined('LDAP_ACCOUNTDOMAINNAME') || define('LDAP_ACCOUNTDOMAINNAME','test.com');
-	           defined('LDAP_ACCOUNTDOMAINNAMESHORT') || define('LDAP_ACCOUNTDOMAINNAMESHORT','TEST');
-	           defined('LDAP_ACCOUNTCANONICALFORM') || define('LDAP_ACCOUNTCANONICALFORM','1');
-	           defined('LDAP_BASEDN') || define('LDAP_BASEDN','OU=users,DC=test,DC=com');
+	           defined('LDAP_USERNAME') || define('LDAP_USERNAME','CN=admin1,CN=Users,DC=domain3,DC=local');
+	           defined('LDAP_PASSWORD') || define('LDAP_PASSWORD','6kjSTXO3QqdCf83');
+	           defined('LDAP_ACCOUNTFILTERFORMAT') || define('LDAP_ACCOUNTFILTERFORMAT','(&(objectClass=user)(cn=%s))');
+	           defined('LDAP_ACCOUNTDOMAINNAME') || define('LDAP_ACCOUNTDOMAINNAME','DT3-AD.domain3.local');
+	           defined('LDAP_ACCOUNTDOMAINNAMESHORT') || define('LDAP_ACCOUNTDOMAINNAMESHORT','DOMAIN3');
+	           defined('LDAP_ACCOUNTCANONICALFORM') || define('LDAP_ACCOUNTCANONICALFORM','3');
+	           defined('LDAP_BASEDN') || define('LDAP_BASEDN','DC=domain3,DC=local');
 	           defined('LDAP_BINDREQUIRESDN') || define('LDAP_BINDREQUIRESDN','true');
-	           defined('LDAP_SUPER_ADMIN_USERNAME') || define('LDAP_SUPER_ADMIN_USERNAME','sentrifugo');
+	           defined('LDAP_SUPER_ADMIN_USERNAME') || define('LDAP_SUPER_ADMIN_USERNAME','admin1');
 	         ?>
