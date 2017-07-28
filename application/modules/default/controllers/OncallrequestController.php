b9e2ce738df865a29541b4b9df807799d448b8f9
go-faustino <go-faustino@users.noreply.github.com>
2017-07-28 01:21:00 +0100

    validate leave and on call request same month: - correct msgarray var name

--- application/modules/default/controllers/OncallrequestController.php
+++ application/modules/default/controllers/OncallrequestController.php
@@ -542,7 +542,7 @@ class Default_OncallrequestController extends Zend_Controller_Action
 		    if($month1 != $month2)
 		    {
 			    $errorflag = 'false';
-			    $messagearray['from_date'] = ' On call for different months should be requested separately.';
+			    $msgarray['from_date'] = ' On call for different months should be requested separately.';
 		    }
 		}
 
